88c4b6be63339afd24c43bf320332964da1ab0a4
Namit Sharda <namit.sharda63@gmail.com>
2020-05-09 08:27:51 +0530

    Update settings.py

--- martor_demo/martor_demo/settings.py
+++ martor_demo/martor_demo/settings.py
@@ -127,7 +127,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
-STATIC_URL = os.path.join(BASE_DIR,"static")
-MEDIA_URL = os.path.join(BASE_DIR, "media")
+STATIC_URL = os.path.join(BASE_DIR,"static/")
+MEDIA_URL = os.path.join(BASE_DIR, "media/")
 STATIC_ROOT = os.path.join(tempfile.gettempdir(), 'martor_static')
 MEDIA_ROOT = os.path.join(tempfile.gettempdir(), 'martor_media')
